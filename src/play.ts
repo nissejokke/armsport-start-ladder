4e5da1902e63c010f39247806fb1ba205c3de579
Johan <johan@macbookair.local>
2023-07-04 15:34:08 +0200

    Four and five players working. Do winner group done first.

--- src/play.ts
+++ src/play.ts
@@ -13,6 +13,13 @@ export interface MatchResult {
     finished: boolean;
 }
 
+export interface PlayerStats {
+    // player: Player;
+    wins: number;
+    losses: number;
+    matches: number;
+}
+
 
 // pick player with worst score (most losses)
 // 1. Pick two player with least matches that havent lost two times
@@ -24,7 +31,7 @@ export async function play(
     const [p1, p2] = pick(players);
     if (!p1 || !p2) {
         if (!p1 && !p2) throw new Error('No winner!?');
-        // console.log('Winner', p1 ? p1.name : p2.name);
+
         const winner = p1 ? p1! : p2!;
         return {
             winner,
@@ -33,7 +40,7 @@ export async function play(
     }
     const winner = await chooseWinner(p1, p2);
     const loser = winner === p1 ? p2 : p1;
-    // console.log(p1.name + ' vs ' + p2.name + ', winner', winner.name);
+
     winner.wins.push(loser);
     loser.losses.push(winner);
     
@@ -51,11 +58,9 @@ export async function play(
 }
 
 function pick(players: Player[]): Player[] {
-    const existsPlayersWithNoMatches = players
-        .map(p => p.wins.length + p.losses.length)
-        .some(count => count === 0);
+    const playersInGroupA = players.filter(p => p.losses.length === 0);
     
-    // console.log(existsPlayersWithNoMatches);
+    // console.log('A', playersInGroupA.length);
     let sortedPlayers = [...players].sort((a, b) => {
         // todo not the same team if can be avoided
 
@@ -65,66 +70,38 @@ function pick(players: Player[]): Player[] {
         const aHasLosses = Boolean(a.losses.length);
         const bHasLosses = Boolean(b.losses.length);
 
-        // if (existsPlayersWithNoMatches) {
-        //     if (aHasLosses && !bHasLosses) return 1;
-        //     if (bHasLosses && !aHasLosses) return -1;
-        // }
-        // if (a.name === 'Viktoria') {
-        //     console.log(b.name, b.wins.length, b.losses.length)
-        // }
-        
+        if (playersInGroupA.length > 1) {
+            if (aHasLosses && !bHasLosses) return 1;
+            if (bHasLosses && !aHasLosses) return -1;
+        }
+
         // least matches
         const matchDiff = aMatchCount - bMatchCount;
         if (matchDiff !== 0) return matchDiff;
         
-
-        // if (aHasLosses && !bHasLosses) return 1;
-        // if (!aHasLosses && bHasLosses) return -1;
-        // if (playersWithNoMatches) {
-            // most wins
-            // const winsDiff = b.wins.length - a.wins.length;
-            // if (winsDiff !== 0) return winsDiff;
-        //     if (!aHasLosses && bHasLosses) return -1;
-        //     if (aHasLosses && !bHasLosses) return 1;
-        // }
-
-        
-        // if (a.name === 'Viktoria') {
-            //     console.log(a);
-            // }
-            // if (b.name === 'Viktoria') {
-                //     console.log(b);
-                // }
-                // const aMatches = a.wins.length + a.losses.length;
-                // if (aMatches === 0 && b.wins.length) return 1;
-                // const bMatches = b.wins.length + b.losses.length;
-                // if (bMatches === 0 && a.wins.length) return -1;
-                
         const aScore = a.wins.length - a.losses.length;
         const bScore = b.wins.length - b.losses.length;
 
-        // if (existsPlayersWithNoMatches) {
-        //     const scoreDiff = bScore - aScore;
-        //     if (scoreDiff !== 0) return scoreDiff;
-        // }
-        // else {
-            // worst score
-            const scoreDiff = aScore - bScore;
-            if (scoreDiff !== 0) return scoreDiff;
-        // }
+        const scoreDiff = aScore - bScore;
+        if (scoreDiff !== 0) return scoreDiff;
 
         // most rest
-        // const restDiff = (b.rest - a.rest);
-        // if (restDiff !== 0) return restDiff;
-
+        const restDiff = (b.rest - a.rest);
+        if (restDiff !== 0) return restDiff;
 
         return 0;
     });
-    // if (existsPlayersWithNoMatches) {
-    //     sortedPlayers = sortedPlayers.filter(p => p.losses.length === 0);
-    //     console.log('yes')
-    // }
 
     const playersStillIn = sortedPlayers.filter(p => p.losses.length < 2);
     return playersStillIn.slice(0, 2);
+}
+
+export function playerStats(players: Player[]): PlayerStats[] {
+    return players.map(player => {
+        return {
+            wins: player.wins.length,
+            losses: player.losses.length,
+            matches: player.wins.length + player.losses.length,
+        }
+    })
 }
--- src/play_test.ts
+++ src/play_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals, assertStrictEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
-import { MatchResult, Player, play } from "./play.ts";
+import { MatchResult, Player, play, playerStats } from "./play.ts";
 
 export interface TestPlayer extends Player {
     strength: number;
@@ -18,7 +18,7 @@ Deno.test("four players", async () => {
         const winner = p1.strength > p2.strength ? p1 : p2;
         return winner;
     }
-    
+
     let results: MatchResult[] = [];
     let result: MatchResult;
     do {
@@ -27,20 +27,11 @@ Deno.test("four players", async () => {
         console.log(result.players?.[0].name, 'vs', result.players?.[1].name, '=>', result.winner.name);
     } while (!result.finished);
 
-    /**
-      Kent
-      Johan
-      Alex
-      Kent
-      Alex
-      Kent
-      Kent
-     */
     assertEquals(results.map(r => r.winner.name), [
-        players[0].name, 
-        players[3].name, 
-        players[1].name, 
         players[0].name,
+        players[3].name,
+        players[0].name,
+        players[1].name,
         players[1].name,
         players[0].name,
         players[0].name
@@ -61,7 +52,7 @@ Deno.test("five players", async () => {
         const winner = p1.strength > p2.strength ? p1 : p2;
         return winner;
     }
-    
+
     let results: MatchResult[] = [];
     let result: MatchResult;
     do {
@@ -70,22 +61,41 @@ Deno.test("five players", async () => {
         console.log(result.players?.[0].name, 'vs', result.players?.[1].name, '=>', result.winner.name);
     } while (!result.finished);
 
-    /**
-      Kent
-      Johan
-      Alex
-      Kent
-      Alex
-      Kent
-      Kent
-     */
-    // assertEquals(results.map(r => r.winner), [
-    //     players[0], 
-    //     players[3], 
-    //     players[1], 
-    //     players[0],
-    //     players[1],
-    //     players[0],
-    //     players[0]
-    // ]);
+    // for (const player of players) {
+    //     console.log(player.name.padEnd(10, ' '), player.wins.length, player.losses.length, player.wins.length + player.losses.length);
+    // }
+
+    // Alexander  2 2 4
+    // Cronblad   4 0 4
+    // Kent       2 2 4
+    // Jonna      0 2 2
+    // Viktoria   0 2 2
+
+    assertEquals(results[results.length - 1].winner.name, players[1].name);
+    assertEquals(playerStats(players), [{
+            wins: 2,
+            losses: 2,
+            matches: 4
+        },
+        {
+            losses: 0,
+            matches: 4,
+            wins: 4,
+        },
+        {
+            losses: 2,
+            matches: 4,
+            wins: 2,
+        },
+        {
+            losses: 2,
+            matches: 2,
+            wins: 0,
+        },
+        {
+            losses: 2,
+            matches: 2,
+            wins: 0,
+        }
+    ]);
 });
