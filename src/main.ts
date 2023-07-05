5c82c14d996797c7272d989139cd0f4f1f18d4b4
Johan <johan@macbookair.local>
2023-07-05 17:46:27 +0200

    Better tree display

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { MatchResult, Player, play } from "./play";
-import { TreeNode, appendTree, printTree, searchTree } from "./tree-node";
+import { TestPlayer } from "./play_test";
+import { TreeNode, appendTree, printTree, searchTree } from "./tree";
 
 
 export interface InitLadderArgs {
@@ -10,6 +11,8 @@ let ctx: CanvasRenderingContext2D | undefined = undefined;
 export async function initLadder(args: InitLadderArgs) {
 
     const canvas = document.getElementById('canvas')! as HTMLCanvasElement;
+    canvas.width = window.outerWidth;
+    canvas.height = window.outerHeight;
     ctx = canvas.getContext('2d')!;
     // const names = [
     //     'Kent Andersson',
@@ -22,33 +25,53 @@ export async function initLadder(args: InitLadderArgs) {
     //     'Dagblad',
     //     'Klubban'
     // ];
-    const names = [
-        'Kent',
-        'Alex',
-        'Johan',
-        'Ledin'
+    const players: TestPlayer[] = [
+        { name: 'Alexander', strength: 4, wins: [], losses: [], rest: 0 },
+        { name: 'Cronblad', strength: 5, wins: [], losses: [], rest: 0 },
+        { name: 'Kent', strength: 3, wins: [], losses: [], rest: 0 },
+        { name: 'Jonna', strength: 1, wins: [], losses: [], rest: 0 },
+        { name: 'Viktoria', strength: 2, wins: [], losses: [], rest: 0 },
     ];
-    const players: Player[] = names.map(name => ({ name, wins: [], losses: [], rest: 0 }));
+
+    // const players: TestPlayer[] = [
+    //     { name: 'Kent', strength: 3, wins: [], losses: [], rest: 0 },
+    //     { name: 'Alex', strength: 2, wins: [], losses: [], rest: 0 },
+    //     { name: 'Ledin', strength: 1, wins: [], losses: [], rest: 0 },
+    //     { name: 'Johan', strength: 1.5, wins: [], losses: [], rest: 0 },
+    // ];
+
+    async function chooseWinner(p1: TestPlayer, p2: TestPlayer): Promise<Player> {
+        const winner = p1.strength > p2.strength ? p1 : p2;
+        return winner;
+    }
+    // async (p1, p2) => players.indexOf(p1) < players.indexOf(p2) ? p1 : p2
+    
+    // const names = [
+    //     'Kent',
+    //     'Alex',
+    //     'Johan',
+    //     'Ledin'
+    // ];
+    // const players: Player[] = names.map(name => ({ name, wins: [], losses: [], rest: 0 }));
  
     const shuffledPlayers = [...players]; //shuffle(players);
 
     const results: MatchResult[] = [];
     let result: MatchResult;
     do {
-        result = await play(players, async (p1, p2) => players.indexOf(p1) < players.indexOf(p2) ? p1 : p2);
+        result = await play(players, chooseWinner as (p1: Player, p2: Player) => Promise<Player>);
         drawMatchResults(result);
-        // buildTreeNode(result);
         results.push(result);
     } while (!result.finished);
 
     const root = buildTree(results.reverse());
-    drawTree(root, 1, 20, 400);
+    drawTree(root);
 }
 
 function buildTree(results: MatchResult[]) {
     const root = appendTree(results[1]);
 
-    for (let i = 2; i < 20; i++) {
+    for (let i = 2; true; i++) {
         const result = results[i];
         if (!result) break;
         console.log(result.players?.[0].name  + ' vs ' + result.players?.[1].name + ' = ' + result.winner.name);
@@ -58,60 +81,71 @@ function buildTree(results: MatchResult[]) {
         console.log(leaves);
         if (leaves.length === 0) throw new Error('No leaves found, was searching for ' + result.winner.name + ' or ' + result.loser?.name + ' in tree');
         appendTree(result, leaves[0]); // first leave should be children rather than parent
-        console.log('tree:');
+        
+        console.log('tree (#' + i + ')');
         printTree(root, (node) => node.data.players!.map(p => p.name).join(' vs '));
     }
 
     return root;
 }
 
-function drawTree(node: TreeNode<MatchResult>, depth, offsetY, offsetX) {
-    
-    let childIndex = 0;
-    
-    function getX(depth, childIndex) {
-        const x = 70 * 1*(5-depth) + (depth === 1 ? 100 : 0);
-        return offsetX + childIndex * x - x/2;
-    }
-    function getY(depth) {
-        return offsetY + (100*(1));
-    }
-    
-    drawNode(node.data.winner.name, offsetY, offsetX);
+function drawTree(node: TreeNode<MatchResult>) {
+    const x = 500;
+    const y = 20;
+    drawTreeInternal(node, 0, y, x);
+}
 
+function drawTreeInternal(node: TreeNode<MatchResult>, depth, offsetY, offsetX) {
+        
+    function getX(depth, childIndex?: number) {
+        const x = 30 * 1*(7-depth) + 100/depth*2;
+        return offsetX + (childIndex !== undefined ? childIndex * x - x/2 : 0);
+    }
+
+    function getY(depth) {
+        return offsetY + (depth * 50);
+    }
+
+    function drawPlayer(playerIndex) {
+        let y = getY(depth + 1);
+        let x = getX(depth + 1, playerIndex);
+
+        ctx!.beginPath();
+        ctx!.moveTo(rootX, rootY);
+        ctx!.lineTo(x, y);
+        ctx!.fill();
+
+        drawNode(node.data.players?.[playerIndex]?.name ?? '?', y, x);
+    }
+
+    const rootX = getX(depth);
+    const rootY = getY(depth);
+    
+    // player and winner is the same so no need to draw both except for on top level
+    if (depth === 0)
+        drawNode(node.data.winner.name, rootY, rootX);
+
+    drawPlayer(0);
+    drawPlayer(1);
+        
+    const winnerIndex = node.data.players?.findIndex(p => p === node.data.winner)!;
+    let childIndex = 0;
     for (const childNode of node.children) {
-        const y = getY(depth);
-        const x = getX(depth, childIndex);
+        // makes sure sub tree is placed under winner
+        if (childNode.data.winner === node.data.winner)
+            childIndex = winnerIndex;
+        else 
+            childIndex = 1 - winnerIndex;
+        
+        const y = getY(depth+1);
+        const x = getX(depth+1, childIndex);
         
         ctx!.beginPath();
-        ctx!.moveTo(offsetX, offsetY);
+        ctx!.moveTo(rootX, rootY);
         ctx!.lineTo(x, y);
         ctx!.fill();
 
-        drawTree(childNode, depth + 1, y, x );
-        childIndex++;
-    }
-
-    if (node.children.length === 0) {
-        let y = getY(depth + 1);
-        let x = getX(depth + 1, 0);
-
-        ctx!.beginPath();
-        ctx!.moveTo(offsetX, offsetY);
-        ctx!.lineTo(x, y);
-        ctx!.fill();
-
-        drawNode(node.data.players?.[0]?.name ?? '?', y, x);
-
-        y = getY(depth + 1);
-        x = getX(depth + 1, 1);
-
-        ctx!.beginPath();
-        ctx!.moveTo(offsetX, offsetY);
-        ctx!.lineTo(x, y);
-        ctx!.fill();
-
-        drawNode(node.data.players?.[1]?.name ?? '?', y, x);
+        drawTreeInternal(childNode, depth + 1, offsetY, x );
     }
 }
 
@@ -126,9 +160,6 @@ function drawNode(text, offsetY = 0, offsetX = 0) {
 
 function drawMatchResults(result: MatchResult) {
     drawMatchResult(result.players?.[0].name  + ' vs ' + result.players?.[1].name + ' = ' + result.winner.name);
-
-    // drawNode(result.players?.[0].name ?? '?');
-    // drawNode(result.players?.[1].name ?? '?');
 }
 
 function drawMatchResult(text: string) {
