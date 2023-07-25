fddeb9aef69e0a9ace1f91e71396e8188bc3447f
Johan <johan@macbookair.local>
2023-07-25 13:24:19 +0200

    Works except in a few places

--- src/draw-tree.ts
+++ src/draw-tree.ts
@@ -33,10 +33,10 @@ export function drawTree({
     const x = calcWidthOfTree(node) + treeLeftMargin;
     // baseOffset + startPos of tree + half height of tree + padding
     const y = treeTopMargin + treeY + height/2 + treeIndex * treePadding;
-    
-    // console.log('depth=', depth);
-    // console.log('width=', calcWidthOfTree(node));
-    // console.log('height=', height);
+    // const subTreeDepth = calc
+    console.log('depth=', depth);
+    console.log('width=', calcWidthOfTree(node));
+    console.log('height=', height);
 
     drawTreeAtPosition(node, depth, x, y, canvas, 0, onMatchResult, nextUp);
 
@@ -48,13 +48,16 @@ function getX(offsetX: number, depth: number) {
 }
 
 function getY(offsetY: number, depth: number, treeDepth: number, childIndex: number | undefined) {
-    const y = (100 * treeDepth)/Math.max(depth * depth * depth * 0.20 + 1, 1);
+    // 5: treeDepth * 100 = 500
+    // 8: treeDepth * treeDepth * 50 = 3200
+
+    const y = (treeDepth * 100)/Math.max(depth * depth * 0.2 + 1, 1);
     return offsetY + (childIndex !== undefined ? childIndex * y - y/2 : 0);
 }
 
 function calcMaxDepthOfTree(node: TreeNode<MatchResult>, depth = 1): number {
     const depths: number[] = [];
-    if (node.children.length == 0)
+    if (!node || node.children.length == 0)
         return depth;
     for (const child of node.children)
         depths.push(calcMaxDepthOfTree(child, depth + 1));
@@ -77,65 +80,7 @@ function calcWidthOfTree(node: TreeNode<MatchResult>) {
 }
 
 function drawTreeAtPosition(node: TreeNode<MatchResult>, treeDepth: number, offsetX: number, offsetY: number, canvas: TreeDrawing, depth: number, onMatchResult: (winner: Player, loser: Player) => void, nextUp?: MatchResult) {
-    const rootX = getX(offsetX, depth);
-    const rootY = getY(offsetY, depth, treeDepth, undefined);
-    
-    // line offset
-    const lineXOffset = 20;
-    const lineYOffset = treeDepth >= 5 ? 9 : 15;
-    const lineSlope = 10;
-
-    function drawPlayer(playerIndex: number) {
-        let x = getX(offsetX, depth + 1);
-        let y = getY(offsetY, depth + 1, treeDepth, playerIndex);
-
-        canvas.line(x-(depth+1 !== treeDepth ? lineXOffset : 10), y+lineYOffset, rootX-lineXOffset-lineSlope, y+lineYOffset);
-        canvas.line(rootX-lineXOffset-lineSlope, y+lineYOffset, rootX-lineXOffset, rootY+lineYOffset);
-        const isNextUpMatch = /*depth === 0 && !node.data.winner &&*/ node.data === nextUp;
-        const cssClass = ['depth-' + depth];
-        if (depth >= 4)
-            cssClass.push('depth-gte-4');
-        if (depth >= 6)
-            cssClass.push('depth-gte-6');
-        if (depth >= 7)
-            cssClass.push('depth-gte-7');
-        canvas.drawName({ x, y, 
-            text: node.data.players?.[playerIndex]?.name ?? '?', 
-            cssClass,
-            onClick: isNextUpMatch ? () => {
-                if (node.data.players?.[playerIndex] && node.data.players?.[1-playerIndex])
-                    onMatchResult(
-                        node.data.players![playerIndex]!, 
-                        node.data.players![1-playerIndex]! 
-                    );
-            } : undefined,
-        });
-    }
-
-    if (depth === 0)
-        canvas.drawName({ 
-            x: rootX, y: rootY, 
-            text: node.data.winner?.name ? node.data.winner?.name : '?', cssClass: [],
-        });
-    if (depth === 0) {
-        canvas.line(rootX-lineXOffset, rootY+lineYOffset, rootX + treeLineXLength, rootY+lineYOffset);
-    }
-
-    drawPlayer(0);
-    drawPlayer(1);
-        
     if (!node.data.players) throw new Error('No node players');
-
-    // players Kent, Cronblad
-    // node data winner undefined
-    // children: Jonna Cronblad, Totte Kent (both winners and players)
-
-    // players Totte Cronblad
-    // children: Kent - Totte
-
-    // players Alexander Totte
-    // children: Alexander - Totte (w)
-    // corrent child index: 1
 
     // determines if child should be placed to the left och right under parent match
     // depends on order of node.data.players and which child won
@@ -148,13 +93,74 @@ function drawTreeAtPosition(node: TreeNode<MatchResult>, treeDepth: number, offs
         const childIndex2 = node.data.players.findIndex(p => node.children[1].data.winner === p);
         childIndices.push(childIndex2);
     }
+    
+    // const subTreeDepth = calcMaxDepthOfTree(node);
+    const rootX = getX(offsetX, depth);
+    const rootY = getY(offsetY, depth, treeDepth, undefined);
+    console.log('treedepth', treeDepth, 'depth', depth, 'y', rootY);
+    console.log('max depth', calcMaxDepthOfTree(node));
+    
+    // line offset
+    const lineXOffset = 20;
+    const lineYOffset = 9;// treeDepth >= 5 ? 9 : 15;
+    const lineSlope = 10;
+
+    function drawPlayer(playerIndex: number) {
+        const subTreeDepth = node.children[childIndices[playerIndex]] ? calcMaxDepthOfTree(node.children[childIndices[playerIndex]]) : (depth+1 !== treeDepth ? 1 : 2);
+
+        let x = getX(offsetX, depth + 1);
+        let y = getY(offsetY, depth + 1, subTreeDepth, playerIndex);
+
+        // horizontal line
+        canvas.line(x-(depth+1 !== subTreeDepth ? lineXOffset : 10), y+lineYOffset, rootX-lineXOffset-lineSlope, y+lineYOffset);
+        // connecting line
+        canvas.line(rootX-lineXOffset-lineSlope, y+lineYOffset, rootX-lineXOffset, rootY+lineYOffset);
+        const isNextUpMatch = /*depth === 0 && !node.data.winner &&*/ node.data === nextUp;
+        canvas.drawName({ x, y, 
+            text: (node.data.players?.[playerIndex]?.name ?? '?') + ' ' + subTreeDepth + ('y=' + Math.round(y)), 
+            cssClass: [depth >= 4 ? 'depth-gte-4' : 'depth-' + depth],
+            onClick: isNextUpMatch ? () => {
+                if (node.data.players?.[playerIndex] && node.data.players?.[1-playerIndex])
+                    onMatchResult(
+                        node.data.players![playerIndex]!, 
+                        node.data.players![1-playerIndex]! 
+                    );
+            } : undefined,
+        });
+    }
+
+    if (depth === 0) {
+        canvas.drawName({ 
+            x: rootX, y: rootY, 
+            text: node.data.winner?.name ? node.data.winner?.name : '?', cssClass: [],
+        });
+        canvas.line(rootX-lineXOffset, rootY+lineYOffset, rootX + treeLineXLength, rootY+lineYOffset);
+    }
+
+    drawPlayer(0);
+    drawPlayer(1);
+        
+
+    // players Kent, Cronblad
+    // node data winner undefined
+    // children: Jonna Cronblad, Totte Kent (both winners and players)
+
+    // players Totte Cronblad
+    // children: Kent - Totte
+
+    // players Alexander Totte
+    // children: Alexander - Totte (w)
+    // corrent child index: 1
+
+    
 
     let i = 0;
     for (const childNode of node.children) {
         const childIndex = childIndices[i];
-        const y = getY(offsetY, depth+1, treeDepth, childIndex);
+        const subTreeDepth = calcMaxDepthOfTree(childNode);
+        const y = getY(offsetY, depth+1, subTreeDepth, childIndex);
         
-        drawTreeAtPosition(childNode, treeDepth, offsetX, y, canvas, depth + 1, onMatchResult);
+        drawTreeAtPosition(childNode, subTreeDepth, offsetX, y, canvas, depth + 1, onMatchResult);
         i++;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -186,7 +186,7 @@ function createCanvas(parent: Element, width?: number, height?: number): CanvasR
     if (height)
         canvas.height = height;
     else
-        canvas.height = window.outerHeight * 2;
+        canvas.height = window.outerHeight * 10;
 
     const ctx = canvas.getContext('2d')!;
     return ctx;
